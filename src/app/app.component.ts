aa06127b92ff5136f6fe7dd9d6107f7599c6dd4b
EDUARDO\Eduardo <ebm7pc@gmail.com>
2017-08-25 22:50:45 -0500

    parte 3 del tour of heroes (Master/Detail)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,95 @@ export class Heroe {
   nombre: string;
 }
 
+const LISTA_HEROES: Heroe[] = [
+  {identif: 1, nombre: 'Supersona'},
+  {identif: 2, nombre: 'Dr. Empanada'},
+  {identif: 3, nombre: 'Doña Gallina'},
+  {identif: 4, nombre: 'Puebloman'},
+  {identif: 5, nombre: 'Superpaisaman'},
+  {identif: 6, nombre: 'Capitán Chibchombia'},
+  {identif: 7, nombre: 'Ultrapato'},
+  {identif: 8, nombre: 'Condor Herido'},
+  {identif: 9, nombre: 'El Divino Niño'},
+  {identif: 10, nombre: 'Sagrado Corazón'}
+];
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{titulo}}</h1>
-    <h2>Detalles de {{heroe.nombre}}!</h2>
-    <div><label>Identificación: </label>{{heroe.identif}}</div>
+    <h2>Mis Heroes</h2>
+    <ul class="heroes">
+    <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHeroe" (click)="onSelect(hero)">
+      <span class="badge">{{hero.identif}}</span> {{hero.nombre}}
+    </li>
+  </ul>
+  <div *ngIf="selectedHeroe">    
+    <h2>Detalles de {{selectedHeroe.nombre}}!</h2>
+    <div><label>Identificación: </label>{{selectedHeroe.identif}}</div>
     <div>
-      <label>Nombre: </label>
-      <input [(ngModel)]="heroe.nombre" placeholder=nombre>
+      <label>Nombre: </label><input [(ngModel)]="selectedHeroe.nombre" placeholder="nombre">
     </div>
-    `
+  </div>
+    `,
+
+    styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
+    
 })
+
 export class AppComponent  {
-  name = 'Eduardo';
   titulo= 'Tour De Los Heroes';
-  heroe: Heroe = {
-    identif: 1,
-    nombre: 'WindStorm'
+  heroes = LISTA_HEROES;
+  selectedHeroe: Heroe;
+  onSelect(hero: Heroe): void {
+    this.selectedHeroe = hero
+  }
   };
-}
